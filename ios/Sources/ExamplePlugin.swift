05d9d591a626a409df8e3c76bcf1d2aa39cf409b
Justin Waltrip <justin.waltrip@protonmail.com>
2024-12-21 21:46:48 -0500

    Add iCloud support

--- ios/Sources/ExamplePlugin.swift
+++ ios/Sources/ExamplePlugin.swift
@@ -12,9 +12,25 @@ class ExamplePlugin: Plugin {
     let args = try invoke.parseArgs(PingArgs.self)
     invoke.resolve(["value": args.value ?? ""])
   }
+  @objc public func isICloudAvailable(_ invoke: Invoke) {
+    if let iCloudURL = FileManager.default.url(forUbiquityContainerIdentifier: nil) {
+      // iCloud Documents directory is available
+      invoke.resolve([
+        "available": true,
+        "message": "iCloud is available and working",
+        "documentsURL": iCloudURL.absoluteString
+      ])
+    } else {
+      // iCloud is not available or not set up
+      invoke.resolve([
+        "available": false,
+        "message": "iCloud is not available or not configured for this app"
+      ])
+    }
+  }
 }
 
-@_cdecl("init_plugin_icloud")
+@_cdecl("init_plugin_tauri_plugin_icloud")
 func initPlugin() -> Plugin {
   return ExamplePlugin()
 }
